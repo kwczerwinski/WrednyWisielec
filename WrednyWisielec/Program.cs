c10be38b27339e5579ab05ac387bd445bf1a2666
kwczerwinski <kwczerwinski@outlook.com>
2019-01-16 20:00:52 +0100

    Utworzenie programu w Microsoft Visual Studio

---
+++ WrednyWisielec/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WrednyWisielec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Program stworzony na potrzeby zajęć Programowanie Komputerów
+            //Wyższa Szkoła Bankowa w Gdańsku, kierunek Informatyka, semestr I
+            //Wykonanie: Krzysztof Czerwinski
+
+
+        }
+    }
+}
