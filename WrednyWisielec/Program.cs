7c12044b7221666eafac9a638aa7fcb2c118c924
kwczerwinski <kwczerwinski@outlook.com>
2019-01-16 20:28:14 +0100

    Wprowadzenie schematu przebiegu programu.

--- WrednyWisielec/Program.cs
+++ WrednyWisielec/Program.cs
@@ -14,7 +14,49 @@ namespace WrednyWisielec
             //Wyższa Szkoła Bankowa w Gdańsku, kierunek Informatyka, semestr I
             //Wykonanie: Krzysztof Czerwinski
 
+            //Wczytanie słów w pliku tekstowego do tablicy
 
+            //Pętla z grą
+
+            //1. Wybór litery przez gracza
+
+            //2. Weryfikacja ilości słów nie zawierających wybranych liter
+
+            //3. Jeśli ilość słów > 0 i ilość prób gracza > 0 to,
+
+            //3.1. Wprowadź literę do tablicy liter
+
+            //3.2. Obniż ilość prób gracza o 1
+
+            //3.3. Jeśli ilość prób gracza = 0, idź do 12.
+
+            //3.4. Wróć do punktu 1
+
+            //4. Losuj słowo, które nie zawiera liter z tablicy liter.
+
+            //5. Wypisz pozycje ostatniej litery wybranej przez gracza.
+
+            //6. Jeśli ilość prób gracza = 0, idź do 12.
+
+            //7. Wybór litery przez gracza.
+
+            //8. Jeśli litera jest w danym słowie:
+
+            //8.1. Wypisz pozycje litery w danym słowie
+
+            //8.2. Jeśli słowo zostało odkryte, idź do 13.
+
+            //8.3. Wróć do 7.
+
+            //9. Obniż ilość prób gracza o 1.
+
+            //10. Jeśli ilość prób gracza = 0. idź do 12.
+
+            //11. Wróć do 7.
+
+            //12. Wypis przegranej gracza.
+
+            //13. Wypis wygranej gracza.
         }
     }
 }
